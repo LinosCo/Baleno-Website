9f1318aaa8b467125c2c1e5fa2a215c6a7c15ce6
Alessandro Borsato <alessandroborsato@Host-007.homenet.telecomitalia.it>
2025-11-14 11:04:00 +0100

    UX: Migliorata differenziazione visiva tra pulsante Dashboard (outline) e Logout (con icona)

--- apps/web/src/components/UserNavbar.tsx
+++ apps/web/src/components/UserNavbar.tsx
@@ -46,7 +46,8 @@ export default function UserNavbar() {
             <>
               <Link
                 href="/dashboard"
-                className="btn btn-light btn-sm text-primary fw-semibold d-none d-md-inline-flex align-items-center gap-2"
+                className="btn btn-outline-light btn-sm fw-semibold d-none d-md-inline-flex align-items-center gap-2"
+                style={{ borderWidth: '1.5px' }}
               >
                 <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
@@ -58,8 +59,12 @@ export default function UserNavbar() {
               </span>
               <button
                 onClick={handleLogout}
-                className="btn btn-light text-primary fw-semibold px-4"
+                className="btn btn-light text-primary fw-semibold px-4 d-inline-flex align-items-center gap-2"
               >
+                <svg width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
+                  <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                  <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                </svg>
                 Logout
               </button>
             </>
